172badab497e8f37578a9e64914210859a4a66fa
sorgazb <sorgazb01@educarex.es>
2025-12-11 21:43:01 +0100

    Añadir comentarios explicativos en la función para imprimir el Triángulo de Pascal

--- Tema_03/Tareas/Repaso_Funciones/ejercicio_24.py
+++ Tema_03/Tareas/Repaso_Funciones/ejercicio_24.py
@@ -5,7 +5,12 @@ from math import factorial
 def mostrarTrianguloPascal(numeroFilas):
     for fila in range(numeroFilas):
         for elemento in range(fila + 1):
+            # Para calcular el Triangulo de Pasca se puede hacer con una formula que aplica el factorial,
+            # Esta fomula lo que hace es calucular el factorial del nivel del triangulo de pascal en el que
+            # nos encontramos 
+            # Con el end vamos concatenando cada una de las cadenas
             print(factorial(fila) // (factorial(elemento) * factorial(fila - elemento)), end=' ')
+        # Para mostar un salto de linea
         print()
 
 numeroFilas = int(input('Introduce el numero de filas del Triangulo de Pascal: '))
