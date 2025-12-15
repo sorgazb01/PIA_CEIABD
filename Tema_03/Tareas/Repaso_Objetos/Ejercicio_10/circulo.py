6b0b63452ab0f2b1fd7545f05370c88550f91ce9
sorgazb <sorgazb01@educarex.es>
2025-12-15 17:39:59 +0100

    Ejercicio 10 repaso de objetos

--- Tema_03/Tareas/Repaso_Objetos/Ejercicio_10/circulo.py
+++ Tema_03/Tareas/Repaso_Objetos/Ejercicio_10/circulo.py
@@ -4,6 +4,14 @@ import math
 class Circulo(Figura):
     def __init__(self, radio):
         self.radio = radio
-
+    
+    @property
+    def radio(self):
+        return self._radio
+    
+    @radio.setter
+    def radio(self, radio):
+        self._radio = radio
+    
     def area(self):
         return math.pi * (self.radio ** 2)
--- Tema_03/Tareas/Repaso_Objetos/Ejercicio_10/rectangulo.py
+++ Tema_03/Tareas/Repaso_Objetos/Ejercicio_10/rectangulo.py
@@ -4,6 +4,22 @@ class Rectangulo(Figura):
     def __init__(self, base, altura):
         self.base = base
         self.altura = altura
+        
+    @property
+    def base(self):
+        return self._base
+    
+    @base.setter
+    def base(self, base):
+        self._base = base
+
+    @property
+    def altura(self):
+        return self._altura
+    
+    @altura.setter
+    def altura(self, altura):
+        self._altura = altura
 
     def area(self):
         return self.base * self.altura
