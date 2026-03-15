b98738b3ea4dbe7da1a0ed82bd7896de323fcb29
sorgazb <sorgazb@gmail.com>
2026-03-15 20:25:10 +0100

    Ejercicio de Areas

---
+++ Tema_03/Tareas/Modulos_Paquetes/Areas/areas.py
@@ -0,0 +1,10 @@
+import math
+
+def areaRectangulo(base, altura):
+    return base * altura
+
+def areaTriangulo(base, altura):
+    return (base * altura) / 2
+
+def areaCirculo(radio):
+    return math.pi * radio ** 2
---
+++ Tema_03/Tareas/Modulos_Paquetes/Areas/calculos.py
@@ -0,0 +1,8 @@
+import areas
+
+def main():
+    print(f"Área del rectángulo de 5x9: {areas.areaRectangulo(5, 9)}")
+    print(f"Área del triángulo de 5x13: {areas.areaTriangulo(5, 13)}")
+    print(f"Área del círculo de radio 5: {areas.areaCirculo(5):.2f}")
+
+main()
