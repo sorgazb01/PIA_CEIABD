636fc448a8815a63cf2e38b14c218bf03ee0b6fc
sorgazb <sorgazb@gmail.com>
2026-03-15 20:13:05 +0100

    Ejercicios 1 Modulos y Paquetes

---
+++ Tema_03/Tareas/Modulos_Paquetes/Ejercicio_Modulos_Propios/ejercicio_1_ejecucion.py
@@ -0,0 +1,12 @@
+﻿import ejercicio_1_modulo as modulo
+
+def main():
+    lista = modulo.generarLista()
+    print("Lista generada:")
+    modulo.mostrarLista(lista)
+
+    lista_ordenada = modulo.ordenarLista(lista)
+    print("Lista ordenada:")
+    modulo.mostrarLista(lista_ordenada)
+
+main()
---
+++ Tema_03/Tareas/Modulos_Paquetes/Ejercicio_Modulos_Propios/ejercicio_1_modulo.py
@@ -0,0 +1,17 @@
+﻿# Importamos el modulo random
+import random
+
+# Funcion para generar la lista de numeros aleatorios
+def generarLista():
+    listaRandom = []
+    for _ in range(7):
+        listaRandom.append(random.randint(0, 100))
+    return listaRandom
+
+# Funcion para mostrar la lista de numeros
+def mostrarLista(lista):
+    print(lista)
+
+# Funcion para ordenar la lista de numeros
+def ordenarLista(lista):
+    return sorted(lista)
